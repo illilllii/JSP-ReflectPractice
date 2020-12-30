ec9261fecad1db7eae559022470fc45ff1aa1dda
sksdl9749 <sksdl9749@gmail.com>
2020-12-30 17:32:07 +0900

    리플렉션한 메서드의 파라메터 값 주입하기

--- src/com/cos/reflect/filter/Dispatcher.java
+++ src/com/cos/reflect/filter/Dispatcher.java
@@ -1,6 +1,7 @@
 package com.cos.reflect.filter;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
@@ -20,6 +21,8 @@ import com.cos.reflect.controller.UserController;
 
 public class Dispatcher implements Filter {
 
+	private boolean isMatching = false;
+	
 	@Override
 	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
 			throws IOException, ServletException {
@@ -63,6 +66,7 @@ public class Dispatcher implements Filter {
 			//System.out.println(requestMapping.value());
 			
 			if(requestMapping.value().equals(endPoint)) {
+				isMatching = true;
 				try {
 					Parameter[] params = method.getParameters(); // LoginDto
 					String path = null;
@@ -76,12 +80,9 @@ public class Dispatcher implements Filter {
 //						System.out.println("username : " +username);
 //						System.out.println("password : " + password);
 						
-						Enumeration<String> keys = request.getParameterNames(); // username, password
-						
-						while(keys.hasMoreElements()) {
-							System.out.println(keys.nextElement()); 
-						}
-						path = "/";
+						setData(dtoInstance, request);
+					
+						path = (String) method.invoke(userController, dtoInstance);
 					} else {
 						path = (String) method.invoke(userController);
 					}
@@ -95,6 +96,49 @@ public class Dispatcher implements Filter {
 				break;
 			}
 		}
+		if(isMatching == false) {
+			response.setContentType("text/html;charset=utf-8");
+			PrintWriter out = response.getWriter();
+			out.println("잘못된 주소 요청입니다. 404 에러");
+			out.flush();
+		}
+	}
+	
+	private <T> void setData(T instance, HttpServletRequest request) {
+		Enumeration<String> keys = request.getParameterNames(); // 크기 : 2
+		
+		
+		while(keys.hasMoreElements()) {
+			String key = (String) keys.nextElement();
+			String methodKey = keyToMethodKey(key); //setUsername
+			
+			Method[] methods = instance.getClass().getDeclaredMethods(); //5개
+			
+			for (Method method : methods) {
+				if(method.getName().equals(methodKey)) {
+					try {
+						method.invoke(instance, request.getParameter(key)); // String
+					} catch (Exception e) {
+						try {
+							int value = Integer.parseInt(request.getParameter(key));
+							method.invoke(instance, value);
+						} catch (Exception e2) {
+							e2.printStackTrace();
+						}
+						System.out.println("신경쓸 필요 없는 int 파싱 문제");
+					}
+				}
+			}
+			
+		}
+	}
+	
+	private String keyToMethodKey(String key) {
+		String firstKey = "set";
+		String upperKey = key.substring(0, 1).toUpperCase();
+		String remainKey = key.substring(1);
+		
+		return firstKey + upperKey + remainKey;
 	}
 
 }
